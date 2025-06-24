bf8cdc7af2baa84c99ddb7f12b8e554ab17f00d9
Bryan Choate <bryan.choate@gmail.com>
2025-06-24 08:41:21 -0400

    Remove common-lib tests; will recreate later

--- common-lib/src/common_lib/models.py
+++ common-lib/src/common_lib/models.py
@@ -1,5 +1,10 @@
-from sqlalchemy.orm import DeclarativeBase
+from typing import Union
+from uuid import UUID
+
+from sqlalchemy import Integer
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
+    id: Mapped[Union[int, UUID]] = mapped_column(Integer, primary_key=True)
--- common-lib/tests/test_models.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-try:
-    from common_lib.models import Base
-except ImportError:
-    pytest.skip("models.Base not available")
-
-def test_models_base_exists() -> None:
-    assert Base is not None

03cf4f9cab4a214837633dbe7af01b3296389e52
Bryan Choate <bryan.choate@gmail.com>
2025-07-29 12:29:28 -0400

    Incremental progress on web-ui registration

--- web-ui/app/main.py
+++ web-ui/app/main.py
@@ -1,10 +1,23 @@
+import os
+
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from starlette.responses import Response
+from starlette.responses import HTMLResponse, Response
 
-app = FastAPI()
+app = FastAPI(
+    title="Web UI",
+    version="1.0.0",
+    docs_url="/web_ui_docs",
+    redoc_url=None,
+    debug=(os.getenv("ENV", "dev") == "dev"),
+)
 templates = Jinja2Templates(directory="templates")
+
+if app.debug:
+    # clear Jinja’s cache so template files are re-read on each render
+    templates.env.auto_reload = True
+    templates.env.cache = {}
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
@@ -12,3 +25,16 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 @app.get("/register")
 async def register_form(request: Request) -> Response:
     return templates.TemplateResponse("register.html", {"request": request})
+
+
+@app.get("/profile", response_class=HTMLResponse)
+async def profile(
+    request: Request,
+    email: str,
+    first_name: str,
+    last_name: str,
+) -> HTMLResponse:
+    user = {"email": email, "first_name": first_name, "last_name": last_name}
+    return templates.TemplateResponse(
+        "profile.html", {"request": request, "user": user}
+    )
